dfb0582c23ac095b5a81086d9cebd8637bf69aef
bessm123123-oss <bessm123123@gmail.com>
2026-08-29 01:05:05 +0300

    Fix preload API used by renderer

--- preload.js
+++ preload.js
@@ -1,18 +1,20 @@
+'use strict';
+
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('tonnageDesktop', {
-  getMeta: () => ipcRenderer.invoke('app:get-meta'),
-  readStore: (name) => ipcRenderer.invoke('store:read', name),
-  writeStore: (name, value) => ipcRenderer.invoke('store:write', name, value),
-  openData: (options) => ipcRenderer.invoke('dialog:open-data', options),
-  readTextFile: (filePath) => ipcRenderer.invoke('file:read-text', filePath),
-  saveText: (options) => ipcRenderer.invoke('dialog:save-text', options),
-  saveBytes: (options) => ipcRenderer.invoke('dialog:save-bytes', options),
-  showItem: (filePath) => ipcRenderer.invoke('shell:show-item', filePath),
-  openPath: (filePath) => ipcRenderer.invoke('shell:open-path', filePath),
-  onExternalOpenFile: (handler) => {
-    const listener = (_event, filePath) => handler(filePath);
-    ipcRenderer.on('external-open-file', listener);
-    return () => ipcRenderer.removeListener('external-open-file', listener);
-  },
+contextBridge.exposeInMainWorld('desktopAPI', {
+  isDesktop: true,
+  platform: process.platform,
+  loadWorkspaceSync: () => ipcRenderer.sendSync('workspace:load-sync'),
+  loadAnalyticsSync: () => ipcRenderer.sendSync('analytics:load-sync'),
+  saveWorkspace: snapshot => ipcRenderer.invoke('workspace:save', snapshot),
+  saveAnalytics: snapshot => ipcRenderer.invoke('analytics:save', snapshot),
+  clearWorkspace: () => ipcRenderer.invoke('workspace:clear'),
+  clearAnalytics: () => ipcRenderer.invoke('analytics:clear'),
+  openDataFile: () => ipcRenderer.invoke('file:open'),
+  savePlanningFile: (text, suggestedName) => ipcRenderer.invoke('file:save-planning', { text, suggestedName }),
+  saveCsvFile: (text, suggestedName) => ipcRenderer.invoke('file:save-csv', { text, suggestedName }),
+  printPdf: suggestedName => ipcRenderer.invoke('file:print-pdf', { suggestedName }),
+  onOpenedFile: callback => ipcRenderer.on('file:opened', (_event, payload) => callback(payload)),
+  onCommand: callback => ipcRenderer.on('app:command', (_event, command) => callback(command))
 });
